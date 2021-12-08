3990ee05053a62a587502844e0db43f2dfc48956
anuj sharma <2000anujsharma@gmail.com>
2021-12-08 15:42:54 +0530

    Add project files.

---
+++ Captchademo/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using Captchademo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Captchademo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Student student = new Student();
+            return View(student);
+        }
+        [HttpPost]
+        public IActionResult Result(Student s)
+        {
+           
+            return View("Result",s);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
